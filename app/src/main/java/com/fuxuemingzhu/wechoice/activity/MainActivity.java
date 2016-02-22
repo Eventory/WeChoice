2fb443ee953ce2714ca98b4c4176b0d351a987ac
负雪明烛 <fuxuemingzhu@163.com>
2016-02-22 14:17:20 +0800

    增加下拉刷新功能

--- app/src/main/java/com/fuxuemingzhu/wechoice/activity/MainActivity.java
+++ app/src/main/java/com/fuxuemingzhu/wechoice/activity/MainActivity.java
@@ -12,19 +12,21 @@ import android.widget.Toast;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
 import com.fuxuemingzhu.wechoice.R;
 import com.fuxuemingzhu.wechoice.adapter.ChoiceAdapter;
 import com.fuxuemingzhu.wechoice.app.BaseActivity;
 import com.fuxuemingzhu.wechoice.entity.Choice;
 import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.Callback;
 import com.zhy.http.okhttp.callback.StringCallback;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import okhttp3.Call;
-import okhttp3.Response;
 
 public class MainActivity extends BaseActivity {
 
@@ -34,6 +36,12 @@ public class MainActivity extends BaseActivity {
 
     private ListView lv_choices;
     private ListAdapter listAdapter;
+
+    private MaterialRefreshLayout materialRefreshLayout;
+    private int pages = 1;
+    final int QUEUE_SIZE = 10;//队列大小
+    //手写队列用来存储已经加载过的文章页数
+    Queue<Integer> autoQueue = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,35 +54,68 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initViews() {
         lv_choices = (ListView) findViewById(R.id.lv_main_choices);
+        materialRefreshLayout = (MaterialRefreshLayout) findViewById(R.id.refresh_main);
     }
 
     @Override
     protected void initEvents() {
         getStringContent();
+        materialRefreshLayout.setLoadMore(true);
+        materialRefreshLayout.finishRefreshLoadMore();
+        materialRefreshLayout.setMaterialRefreshListener(new MaterialRefreshListener() {
+            @Override
+            public void onRefresh(final MaterialRefreshLayout materialRefreshLayout) {
+                while (autoQueue.contains(pages)) {
+                    pages = (int) Math.ceil(Math.random() * 25);
+                }
+                getStringContent();
+            }
+
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+                showCustomToast("onRefreshLoadMore");
+            }
+
+            @Override
+            public void onfinish() {
+                super.onfinish();
+            }
+        });
+
     }
 
     private void getStringContent() {
+        if (autoQueue.size() == QUEUE_SIZE) {
+            autoQueue.poll();
+        }
+        autoQueue.offer(pages);
+        Log.i("autoQueue", autoQueue.toString());
         OkHttpUtils
                 .get()
                 .url(url)
+                .addParams("pno", Integer.toString(pages))
                 .addParams("key", APPKEY)
                 .build()
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e) {
+                        materialRefreshLayout.finishRefresh();
                         Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
                     }
 
                     @Override
                     public void onResponse(String response) {
+                        materialRefreshLayout.finishRefresh();
                         if (response == null) {
-                            Toast.makeText(MainActivity.this, "response is real null", Toast.LENGTH_LONG)
+                            Toast.makeText(MainActivity.this, "response is null", Toast.LENGTH_LONG)
                                     .show();
                             return;
                         }
                         JSONObject responseJson = JSON.parseObject(response);
                         JSONObject result = responseJson.getJSONObject("result");
                         JSONArray jsonList = result.getJSONArray("list");
+                        listChoice.clear();
                         for (int i = 0; i < jsonList.size(); i++) {
                             JSONObject choiceJson = jsonList.getJSONObject(i);
                             Choice choice = JSON.parseObject(choiceJson.toJSONString(), Choice.class);
@@ -100,38 +141,6 @@ public class MainActivity extends BaseActivity {
                             }
                         });
                         Log.i("response", listString);
-                    }
-                });
-    }
-
-    private void getContent() {
-        OkHttpUtils
-                .get()
-                .url(url)
-                .addParams("key", APPKEY)
-                .build()
-                .execute(new Callback() {
-                    @Override
-                    public Object parseNetworkResponse(Response response) throws Exception {
-                        if (response != null) {
-                            Log.i("parseNetworkResponse", response.toString());
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    public void onError(Call call, Exception e) {
-                        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onResponse(Object response) {
-                        if (response == null) {
-                            Toast.makeText(MainActivity.this, "response is real null", Toast.LENGTH_LONG)
-                                    .show();
-                            return;
-                        }
-                        Log.i("response", response.toString());
                     }
                 });
     }
